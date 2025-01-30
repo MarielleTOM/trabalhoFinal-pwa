57f267288b93bac8f42f9c138626089d67bf6db3
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2025-01-30 11:00:58 -0400

    implementado o DB
    tem alguns erro de funcionamento, como a camera n está funcionando ainda, mas o código pra o banco ta no documento

---
+++ js/db.js
@@ -0,0 +1,52 @@
+import { openDB } from "idb";
+let db;
+
+async function createDB( ) {
+    try{
+        db = await openDB('banco', 1 , {
+            upgrade(db, oldeVersion, newVersion, transaction){
+                switch(oldeVersion){
+                    case 0:
+                    case 1: 
+                        const store = db.createObjectStore('pessoas', {
+                            //a propriedade nome será o campo chave
+                            keyPath:'nome'
+                        });
+                        //crinado um indice id na store, deve estar contido no objeto do banco.
+                        store.createIndex('id','id');
+                        showResult('Banco de dados criado!');
+                }
+            }
+        });
+        showResult('Banco de dados aberto');
+    }catch (e) {
+        showResult('Erro ao criar o banco de dados: '+ e.message)
+    }
+}
+
+window.addEventListener('DOMContentLoaded', async event =>{
+    createDB();
+    document.getElementById('input');
+    document.getElementById('btnSalvar').addEventListener('click', addData);
+    document.getElementById('btnListar').addEventListener('click', getData);
+})
+
+async function addData( ) {
+    const tx = await db.transaction('pessoas', 'readwrite');
+    const store = tx.objectStore('pessoas');
+    store.dd({nome: 'Fulano'})
+    await tx.done;
+}
+
+async function getData( ) {
+    if(db == undefined){
+        showResult('O banco de dados está fechado');
+        return;
+    } else {
+        showResult('Não há nenhum dado no banco!')
+    }
+}
+
+function showResult (text){
+    document.querySelector('output').innerHTML = text;
+}
